e7b892d3ecb07cca24499b4d55211ee4d39ebd42
Oori Mark <oorimark@gmail.com>
2022-07-04 20:06:38 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main: prints string
+ *
+ * Return: returns 0 (success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
+
