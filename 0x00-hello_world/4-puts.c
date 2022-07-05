d5951d740d4d653260e6ffbbc5799f1f5884cabc
Oori Mark <oorimark@gmail.com>
2022-07-04 20:19:28 -0700

    fix Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-
 /**
- * main: prints string
+ * main - outputs a string
  *
+ * description: outputs a string
  * Return: returns 0 (success)
  */
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	/**
+	 * description: outputs a string
+	 */
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
