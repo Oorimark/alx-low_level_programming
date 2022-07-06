71125617121f528a2b64fdbbb0c9937e20a55b6f
Oori Mark <oorimark@gmail.com>
2022-07-05 20:40:30 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/** 
+ * print_alphabet - prints alphabets to stdout
+ *
+ * Return: void
+ *
+ */
+void print_alphabet(void)
+{
+	char alphas[26] = "abcdefghijklmnopqrstuvwxyz";	
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(alphas[i]);
+	}
+
+	_putchar('\n');
+}
+
