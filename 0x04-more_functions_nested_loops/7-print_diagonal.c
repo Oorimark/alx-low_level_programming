6f2290c324478b89d4db63f2b2e75ec4a93cea85
Oori Mark <oorimark@gmail.com>
2022-07-07 23:28:11 -0700

    print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagnoally
+ * @n: must be int
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int tracker = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (tracker < n)
+		{
+			int xtracker = -1;
+
+			while (xtracker < tracker)
+			{
+				_putchar(' ');
+				xtracker++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			tracker++;
+		}
+	}
+}
