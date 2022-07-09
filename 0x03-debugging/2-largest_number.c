1cf4eb84472a3d4a59356f1cbb354bc6fd29536e
Oori Mark <oorimark@gmail.com>
2022-07-08 22:09:50 -0700

    fix largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+int max(int [], int);
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
@@ -11,20 +11,22 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-
-	if (a > b && b > c)
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else if ( c > a && a > b)
-	{
-		largest = c;
-	}
-
+	int arr[] = {a,b,c};
+	int n = 3;
+	largest = max(arr,n);
 	return (largest);
+
+}
+
+int max(int x[], int k)
+{
+	int t,i;
+	t = x[0];
+	for (i = 1; i < k; i++)
+	{
+		if (x[i] > t)
+			t=x[i];
+	}
+	return (t);
 }
 
