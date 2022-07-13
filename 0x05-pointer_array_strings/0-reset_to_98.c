a7874602c33ebfc9e17ce054c9219fe888f55d9e
Oori Mark <oorimark@gmail.com>
2022-07-13 00:01:51 -0700

    reset to 98

---
+++ 0x05-pointer_array_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/** 
+ * reset_to_98 - changes the value of the adress given
+ * @*n: arguments is an address
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointer_array_strings/main.h
@@ -0,0 +1,3 @@
+/** All prototypes goes here */
+
+void reset_to_98(int *n);
