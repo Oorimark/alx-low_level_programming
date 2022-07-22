5ff65bdc5bb52d02dba5eb75fa85dcb43ffc9036
Oori Mark <oorimark@gmail.com>
2022-07-21 19:42:45 -0700

    prints length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - stdout lenght of string
+ * @s: string value
+ *
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
