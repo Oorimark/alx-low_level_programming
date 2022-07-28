015b41265b62596d5bf6979122ba5e9676259da9
Oori Mark <oorimark@gmail.com>
2022-07-27 19:30:14 -0700

    multiply two arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the product of two numbers
+ * @argc: argument length
+ * @argv: argument values
+ *
+ * Return: Always 0 | 1
+ */
+int main(int argc, char *argv[])
+{
+	int res;
+
+	if (argc > 1)
+	{
+		res = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", res);
+		return (0);
+	}
+	else
+	{
+		puts("Error \n");
+		return (1);
+	}
+}
